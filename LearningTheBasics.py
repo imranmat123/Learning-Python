dc60d5bf76144c6546fbd87a65188f8e5133e71d
Imran <imranmatloob@live.co.uk>
2025-10-19 11:03:25 +0100

    python code

---
+++ LearningTheBasics.py
@@ -0,0 +1,10 @@
+#our first line of code:
+print("hello python world")
+
+#our first varible:
+message = "hello python world"
+print(message)
+
+#our first bug:
+mesage = "this is our first bug"
+print(mesage)
---
+++ simple_message.py
@@ -0,0 +1,5 @@
+message = "hi my name is imran"
+print(message)
+
+message = "turns out I am a cat, my name is  basil 'lol'"
+print(message)
