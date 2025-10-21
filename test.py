5219a4be34426085a6b36bc3005a679649be059d
imranmat123 <imranmatloob123@gmail.com>
2025-10-21 13:51:39 +0100

    removing suffix and prefix

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-asdfghjk
+endLoop = ""
+GuessTheCountry = ""
+country = "england"
+while endLoop != "Q":
+    GuessTheCountry = input("Please Guess the Country")
+    if country == GuessTheCountry:
+        print("well done you have won!")
+        endLoop = input("do you want to try again? Press Q to quit, or anything else to play again")
+    else:
+        print("try again")
+
+print("Thank you for playing")
