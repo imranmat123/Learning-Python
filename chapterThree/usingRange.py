7e465f9bfdf79df08101ce051619913769ffe354
imranmat123 <imranmatloob123@gmail.com>
2025-10-27 18:00:32 +0000

    working with loops

---
+++ chapterThree/usingRange.py
@@ -0,0 +1,19 @@
+for numbers in range(1,5):
+    print(numbers)
+print(numbers)
+
+square = []
+
+print("\n")
+
+for value in range(1,11):
+    square.append(value **2)
+print(square)
+
+print("\n")
+
+square = []
+for value in range(1,11,5):
+    square.append(value **2)
+
+print(square)
