c2a6110b30eaa1aeb8f5143bca59027a2d895102
imranmat123 <imranmatloob123@gmail.com>
2025-11-17 19:05:58 +0000

    learning about functions

---
+++ chapter8/fun_with_functions.py
@@ -0,0 +1,19 @@
+def display_message():
+	print("in this chapter we will be learning "
+		  "about functions and how they work")
+
+display_message()
+
+def fav_book(title):
+	print(f"my favorite book is called {title.title()}")
+
+fav_book("harry potter")
+
+
+def define_pet(pet_name, pet_type):
+	print(f"I have a {pet_type.title()}")
+	print(f"My {pet_type.title()} is named {pet_name.title()}")
+
+
+define_pet("basil", "cat")
+define_pet("Bhuna", "cat")
